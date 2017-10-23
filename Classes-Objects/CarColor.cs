1b4c419c73ea7bd8e73ffc1bdb05c70aa509eef7
meRahulSingh <mr.rahulsingh@hotmail.com>
2017-10-23 13:51:44 +0200

    change applied

---
+++ Classes-Objects/CarColor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_Objects
+{
+    class CarColor
+    {
+        private string color;
+        public void Color(string newColor)
+        {
+            Console.WriteLine("This is a test");
+            Console.WriteLine("This is a test");
+        }
+            
+        }
+
+
+    }
+
